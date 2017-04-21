18528bb98dc15f2e3d24585543f19070ecdf320e
prachi7792 <prachipednekar85@gmail.com>
2017-04-22 00:21:40 +0530

    Prediction
    In this program we have to predict what will be the solution

---
+++ Java Assignment 3_3/src/com/acadgild/javaassignment3_3/core/Prediction.java
@@ -0,0 +1,16 @@
+package com.acadgild.javaassignment3_3.core;
+
+
+public class Prediction {
+ 
+	 int x; int y;
+	 String name;
+	 public static void main(String args[]) {
+	 Prediction pnt = new Prediction();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	} }
+
+
+//correct option D
+
+//Explanation:Instance variable of type int and String are initialized to 0 and null respectively.
